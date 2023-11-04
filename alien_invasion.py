392b501b498b2dcebe79e1d39e46ff1ad61b7300
demiapollo <apollodemirel67@gmail.com>
2023-11-03 19:22:53 -0700

    empty Pygame window created

--- alien_invasion.py
+++ alien_invasion.py
@@ -8,7 +8,7 @@ class AlienInvasion:
         """Initialize the game, and create game resources."""
         pygame.init()
 
-        self.screen = pygame.diplay.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
 
     def run_game(self):
